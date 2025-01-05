feec62038c178e8eda9d95e75202cb29c1b4de6a
Yash Makwana <yashmakwana6353@gmail.com>
2025-01-05 22:24:04 +0530

    Done With Creating User UI

--- frontend/src/components/LookingForDriver.jsx
+++ frontend/src/components/LookingForDriver.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const ConfirmRide = (props) => {
+const LookingForDriver = (props) => {
   return (
     <div>
-      <h5 className="p-1 text-center w-[93%] absolute top-0" onClick={()=>props.setConfirmRidePanel(false)}><i className="text-3xl text-gray-200 ri-arrow-down-wide-line"></i></h5>
-      <h3 className="text-2xl font-semibold mb-5">Confirm Your Ride</h3>
+      <h5 className="p-1 text-center w-[93%] absolute top-0" onClick={() => props.setVehicleFound(false)}><i className="text-3xl text-gray-200 ri-arrow-down-wide-line"></i></h5>
+      <h3 className="text-2xl font-semibold mb-5">Looking for a Driver!</h3>
       <div className='flex gap-2 justify-between flex-col items-center'>
         <img className='h-20' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_638,w_956/v1555367349/assets/d7/3d4b80-1a5f-4a8b-ac2b-bf6c0810f050/original/Final_XL.png" alt="" />
         <div className='w-full mt-5'>
@@ -16,27 +16,24 @@ const ConfirmRide = (props) => {
             </div>
           </div>
           <div className='flex items-center gap-5 p-3 border-b-2'>
-          <i className='text-lg ri-map-pin-2-fill'></i>
+            <i className='text-lg ri-map-pin-2-fill'></i>
             <div>
               <h3 className='text-lg font-medium'>562/11-A</h3>
               <p className='text-base text-gray-600 -mt-1'>Kankariya Talab, Bhopal</p>
             </div>
           </div>
           <div className='flex items-center gap-5 p-3'>
-          <i className='text-lg ri-currency-line'></i>
+            <i className='text-lg ri-currency-line'></i>
             <div>
               <h3 className='text-lg font-medium'>₹193.20</h3>
               <p className='text-base text-gray-600 -mt-1'>Cash Cash</p>
             </div>
           </div>
         </div>
-        <button onClick={()=>{
-          props.setVehicleFound(true)
-          props.setConfirmRidePanel(false)
-        }} className='w-full mt-5 bg-green-600 text-white font-semibold p-2 rounded-lg'>Confirm</button>
+        {/* <button className='w-full mt-5 bg-green-600 text-white font-semibold p-2 rounded-lg'>Confirm</button> */}
       </div>
     </div>
   )
 }
 
-export default ConfirmRide
+export default LookingForDriver
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,9 @@ import { gsap } from 'gsap';
 import 'remixicon/fonts/remixicon.css'
 import LocationSearchPanel from "../components/LocationSearchPanel";
 import VehiclePanel from "../components/VehiclePanel";
+import ConfirmRide from "../components/ConfirmRide";
+import LookingForDriver from "../components/LookingForDriver";
+import WaitForDriver from "../components/WaitForDriver";
 
 const Home = () => {
     const [pickup, setPickup] = useState('');
@@ -11,59 +14,92 @@ const Home = () => {
     const [panelOpen, setPanelOpen] = useState(false);
     const vehiclePanelRef = useRef(null);
     const confirmRidePanelRef = useRef(null);
+    const vehicleFoundRef = useRef(null);
+    const driverFoundRef = useRef(null);
     const panelRef = useRef(null);
     const panelCloseRef = useRef(null);
     const [vehiclePanelOpen, setVehiclePanelOpen] = useState(false);
-    const [confirmRidePanel, setConfirmRidePanel] = useState(false)
+    const [confirmRidePanel, setConfirmRidePanel] = useState(false);
+    const [vehicleFound, setVehicleFound] = useState(false);
+    const [driverFound, setDriverFound] = useState(false);
+
+
+
     const submitHandler = () => {
         e.preventDefault();
     }
 
-    useGSAP(function(){
-        if(panelOpen==true){
+    useGSAP(function () {
+        if (panelOpen == true) {
             gsap.to(panelRef.current, {
                 height: '70%',
-                opacity:1
-            })
-            gsap.to(panelCloseRef.current,{
                 opacity: 1
             })
-        }else{
+            gsap.to(panelCloseRef.current, {
+                opacity: 1
+            })
+        } else {
             gsap.to(panelRef.current, {
                 height: '0%',
                 opacity: 0
             })
-            gsap.to(panelCloseRef.current,{
+            gsap.to(panelCloseRef.current, {
                 opacity: 0
             })
         }
     }, [panelOpen]);
 
-    useGSAP(function(){
-        if(vehiclePanelOpen==true){
+    useGSAP(function () {
+        if (vehiclePanelOpen == true) {
             gsap.to(vehiclePanelRef.current, {
                 transform: 'translateY(0)'
             })
-        }else{
+        } else {
             gsap.to(vehiclePanelRef.current, {
                 transform: 'translateY(100%)'
             })
         }
 
-    },[vehiclePanelOpen]);
+    }, [vehiclePanelOpen]);
 
-    useGSAP(function(){
-        if(vehiclePanelOpen==true){
-            gsap.to(vehiclePanelRef.current, {
+    useGSAP(function () {
+        if (confirmRidePanel) {
+            gsap.to(confirmRidePanelRef.current, {
                 transform: 'translateY(0)'
             })
-        }else{
-            gsap.to(vehiclePanelRef.current, {
+        } else {
+            gsap.to(confirmRidePanelRef.current, {
                 transform: 'translateY(100%)'
             })
         }
 
-    },[vehiclePanelOpen])
+    }, [confirmRidePanel])
+
+    useGSAP(function () {
+        if (vehicleFound) {
+            gsap.to(vehicleFoundRef.current, {
+                transform: 'translateY(0)'
+            })
+        } else {
+            gsap.to(vehicleFoundRef.current, {
+                transform: 'translateY(100%)'
+            })
+        }
+
+    }, [vehicleFound]);
+
+    useGSAP(function () {
+        if (driverFound) {
+            gsap.to(driverFoundRef.current, {
+                transform: 'translateY(0)'
+            })
+        } else {
+            gsap.to(driverFoundRef.current, {
+                transform: 'translateY(100%)'
+            })
+        }
+
+    }, [driverFound])
     return (
         <div className="h-screen overflow-hidden">
             <img className="w-16 absolute left-5 top-5" src="https://e7.pngegg.com/pngimages/631/1023/png-clipart-logo-brand-product-design-font-uber-logo-text-logo.png" alt="" />
@@ -72,7 +108,7 @@ const Home = () => {
             </div>
             <div className="flex flex-col justify-end h-screen absolute top-0 w-full">
                 <div className="h-[30%] p-6 bg-white relative">
-                    <h5 ref={panelCloseRef} onClick={()=>{
+                    <h5 ref={panelCloseRef} onClick={() => {
                         setPanelOpen(false)
                     }} className="absolute opacity-0 top-6 text-2xl right-6">
                         <i className="ri-arrow-down-wide-line"></i>
@@ -80,22 +116,28 @@ const Home = () => {
                     <h4 className="text-2xl font-semibold">Find a trip!</h4>
                     <form onSubmit={(e) => { submitHandler(e) }}>
                         <div className="line absolute h-16 w-1 top-[34%] left-10 bg-black rounded-full"></div>
-                        <input value={pickup} onClick={()=>{setPanelOpen(true)}} onChange={(e) => { setPickup(e.target.value) }} className="bg-[#eee] px-12 py-2 text-base rounded-lg w-full mb-3" type="text" placeholder="Add a Pick-up Location" />
-                        <input value={destinantion} onClick={()=>{setPanelOpen(true)}} onChange={(e) => { setDestinantion(e.target.value) }} className="bg-[#eee] px-12 py-2 text-base rounded-lg w-full mb-3" type="text" placeholder="Add a Drop-off Location" />
+                        <input value={pickup} onClick={() => { setPanelOpen(true) }} onChange={(e) => { setPickup(e.target.value) }} className="bg-[#eee] px-12 py-2 text-base rounded-lg w-full mb-3" type="text" placeholder="Add a Pick-up Location" />
+                        <input value={destinantion} onClick={() => { setPanelOpen(true) }} onChange={(e) => { setDestinantion(e.target.value) }} className="bg-[#eee] px-12 py-2 text-base rounded-lg w-full mb-3" type="text" placeholder="Add a Drop-off Location" />
                     </form>
                 </div>
                 <div ref={panelRef} className="bg-white h-0">
-                    {<LocationSearchPanel panelOpen={panelOpen} setPanelOpen={setPanelOpen} vehiclePanelOpen={vehiclePanelOpen} setVehiclePanelOpen={setVehiclePanelOpen}/>}
+                    {<LocationSearchPanel panelOpen={panelOpen} setPanelOpen={setPanelOpen} vehiclePanelOpen={vehiclePanelOpen} setVehiclePanelOpen={setVehiclePanelOpen} />}
                 </div>
             </div>
             <div ref={vehiclePanelRef} className="fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-14">
-              <VehiclePanel setVehiclePanelOpen={setVehiclePanelOpen}/>
+                <VehiclePanel setConfirmRidePanel={setConfirmRidePanel} setVehiclePanelOpen={setVehiclePanelOpen} />
             </div>
             <div ref={confirmRidePanelRef} className="fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-14">
-              <VehiclePanel setVehiclePanelOpen={setVehiclePanelOpen}/>
+                <ConfirmRide setConfirmRidePanel={setConfirmRidePanel} setVehicleFound={setVehicleFound} />
+            </div>
+            <div ref={vehicleFoundRef} className="fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-14">
+                <LookingForDriver setVehicleFound={setVehicleFound} />
+            </div>
+            <div ref={driverFoundRef} className="fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-14">
+                <WaitForDriver setDriverFound={setDriverFound}/>
             </div>
         </div>
     )
 }
 
-export default Home;
+export default Home; ``
